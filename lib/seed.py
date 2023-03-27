4f1b0792c78c198afbc56d1dbf230759cd8287bf
Daniele Lesniowski Zils <lesniowskidaniele@gmail.com>
2023-03-27 16:50:33 -0400

    cli thoughts, plus customer seed

--- lib/seed.py
+++ lib/seed.py
@@ -16,6 +16,21 @@ if __name__ == '__main__':
     d_2 = Drinks(name="Gentle Christmas", description="lavendar and mint", hot=True, size="Small", price=7) 
     d_3 = Drinks(name="Matcha Latte", description="matcha", hot=True, size="Small", price=6)
 
+
+# Customers(Base):
+#     __tablename__ = 'customers'
+
+#     id = Column(Integer(), primary_key=True)
+#     name = Column(String(), index=True)
+#     email = Column(String())
+#     phone = Column(Integer())
+
+    #customers:
+    c_1 = Customers(name="Mark Twain", email="mark_twain@cloud.com", phone=2234554343)
+    c_2 = Customers(name="Sarah Johnson", email="sarahj@cloud.com", phone=2254545656)
+    c_3 = Customers(name="Blair Rich", email="blair_rich@cloud.com", phone=2234552323)
+    
+
     session.add_all([d_1, d_2, d_3])
     session.commit()
     # drinks.append(d_1)
